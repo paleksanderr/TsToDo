141944ac53653bdecc5c471836ec4d9b8473025d
Pawel <pawel.aleksander@gmail.com>
2022-05-03 09:38:19 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Task } from "./models/task";
+import { NewTaskForm } from "./components/NewTaskForm";
+import { TasksList } from "./components/TaskList";
+
+interface State {
+  newTask: Task;
+  tasks: Task[];
+}
+
+class App extends Component<{}, State> {
+  state = {
+    newTask: {
+      id: 1,
+      name: "",
+    },
+    tasks: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Hello React TS!</h2>
+        <NewTaskForm
+          task={this.state.newTask}
+          onAdd={this.addTask}
+          onChange={this.handleTaskChange}
+        />
+        <TasksList tasks={this.state.tasks} onDelete={this.deleteTask} />
+      </div>
+    );
+  }
+
+  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.setState((previousState) => ({
+      newTask: {
+        id: previousState.newTask.id + 1,
+        name: "",
+      },
+      tasks: [...previousState.tasks, previousState.newTask],
+    }));
+  };
+
+  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTask: {
+        ...this.state.newTask,
+        name: event.target.value,
+      },
+    });
+  };
+
+  private deleteTask = (taskToDelete: Task) => {
+    this.setState((previousState) => ({
+      tasks: [
+        ...previousState.tasks.filter((task) => task.id !== taskToDelete.id),
+      ],
+    }));
+  };
 }
 
 export default App;
